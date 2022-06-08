91e11c4ee5ee93f834155493cdde8a083f8276e9
nely de faria <nelly.nf54@gmail.com>
2022-06-08 16:29:28 -0300

    Usando construtor + instanciando novo objeto

--- poo-php/teste-produto.php
+++ poo-php/teste-produto.php
@@ -2,18 +2,19 @@
 
 include 'Produto.php';
 
-$produto1 = new Produto();
-$produto1->definirNome('Tênis para corrida');
-$produto1->definirValor(299);
+$produto1 = new Produto('Tênis para corrida', 299);
+// $produto1->definirNome('Tênis para corrida');
+// $produto1->definirValor(299);
 
-$produto2 = new Produto();
-$produto2->definirNome('Calça Jeans');
+$produto2 = new Produto('Calça Jeans', 100);
+// $produto2->definirNome('Calça Jeans');
 
 try {
-    $produto2->definirValor(-100);
+    $produto2->definirValor(-120);
 } catch (Exception $e) {
     echo $e->getMessage();
 }
 
+$produto3 = new Produto('Bermuda', 150);
 
-var_dump($produto1, $produto2);
+var_dump($produto1, $produto2, $produto3);
