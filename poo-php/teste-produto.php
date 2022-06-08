9be4d9d6020544de2e2efaec2143e359080f71c6
nely de faria <nelly.nf54@gmail.com>
2022-06-08 17:09:40 -0300

    Incluindo categoria aos produtos

--- poo-php/teste-produto.php
+++ poo-php/teste-produto.php
@@ -2,12 +2,13 @@
 
 include 'Produto.php';
 
-$produto1 = new Produto('Tênis para corrida', 299);
-// $produto1->definirNome('Tênis para corrida');
-// $produto1->definirValor(299);
+$categoria1 = new Categoria('Roupas', 'Roupas Unissex');
+$categoria2 = new Categoria('Roupas de banho', 'Toalhas, toalhas para o rosto, etc.');
+$categoria3 = new Categoria('Calçados', 'Calçados em geral');
 
-$produto2 = new Produto('Calça Jeans', 100);
-// $produto2->definirNome('Calça Jeans');
+$produto1 = new Produto('Tênis para corrida', 299, $categoria3);
+
+$produto2 = new Produto('Calça Jeans', 100, $categoria1);
 
 try {
     $produto2->definirValor(-120);
@@ -15,6 +16,6 @@ try {
     echo $e->getMessage();
 }
 
-$produto3 = new Produto('Bermuda', 150);
+$produto3 = new Produto('Bermuda', 150, $categoria1);
 
 var_dump($produto1, $produto2, $produto3);
