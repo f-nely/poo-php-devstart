a626a9ae89ffd118f855ab451492fdea71f79411
nely de faria <nelly.nf54@gmail.com>
2022-06-08 16:27:59 -0300

    Cria construtor + atributo descricao + métodos acessores

--- poo-php/Produto.php
+++ poo-php/Produto.php
@@ -6,6 +6,14 @@ class Produto
 {
     private string $nome;
     private float $valor;
+    private string $descricao;
+
+    public function __construct(string $nome, float $valor)
+    {
+        $this->nome = $nome;
+        $this->valor = $valor;
+        $this->definirValor($valor);
+    }
 
     public function obterNome(): string
     {
@@ -17,11 +25,26 @@ class Produto
         $this->nome = $nome;
     }
 
+    public function obterValor(): float
+    {
+        return $this->valor;
+    }
+
     public function definirValor(float $valor): void
     {
         if ($valor < 0) {
            throw new Exception('Ops, valor não pode ser negativo.');
         }
         $this->valor = $valor;
+    }
+
+    public function obterDescricao(): string
+    {
+        return $this->descricao;
+    }
+
+    public function definirDescricao(string $descricao): void
+    {
+        $this->descricao = $descricao;
     }
 }
