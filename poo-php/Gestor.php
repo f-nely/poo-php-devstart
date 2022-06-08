658f5fb5304ba19bc9d5e172ee4809d539852c61
nely de faria <nelly.nf54@gmail.com>
2022-06-08 17:52:39 -0300

    Criando arquivo Gestor.php

---
+++ poo-php/Gestor.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+class Gestor extends Usuario
+{
+    private float $salario;
+    private string $horario;
+
+    public function obterSalario(): float
+    {
+        return $this->salario;
+    }
+
+    public function definirSalario(float $salario): void
+    {
+        $this->salario = $salario;
+    }
+
+    public function obterHorario(): string
+    {
+        return $this->horario;
+    }
+
+    public function definirHorario(float $horario): void
+    {
+        $this->horario = $horario;
+    }
+}
