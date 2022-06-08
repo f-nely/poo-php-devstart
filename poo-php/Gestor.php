866cc4f6526b385adcfb1507b8dd4546c8df2870
nely de faria <nelly.nf54@gmail.com>
2022-06-08 18:16:19 -0300

    Fazendo sobrescrita do método contrutor

--- poo-php/Gestor.php
+++ poo-php/Gestor.php
@@ -7,6 +7,13 @@ class Gestor extends Usuario
     private float $salario;
     private string $horario;
 
+    public function __construct(string $email, string $senha, float $salario)
+    {
+        parent::definirEmail($email);
+        parent::definirSenha($senha);
+        $this->salario = $salario;
+    }
+
     public function obterSalario(): float
     {
         return $this->salario;
